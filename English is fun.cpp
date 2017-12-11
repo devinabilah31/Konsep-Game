050de94c4fde500d06715e81e9a2dab41e566b80
devinabilah31 <33478868+devinabilah31@users.noreply.github.com>
2017-12-11 08:40:17 +0700

    Add files via upload

---
+++ English is fun.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int nilai1;
+	cout<< "*****************************************"<< endl;
+	cout<< "*                                       *"<< endl;
+	cout<< "* Hi Fellas! Welcome to English is Fun! *"<< endl;
+	cout<< "*                                       *"<< endl;
+	cout<< "*****************************************"<< endl;
+	cout<< "*****************************************"<< endl;
+	cout<< "This game bring to you by SeptemberFellas"<< endl;
+	cout<< "****************aka Devis****************"<< endl;
+	cout<< "*                                       *"<< endl;
+	cout<< "*                                       *"<< endl;
+	cout<< "*****************************************"<< endl;
+	cout<< "Welcome to the Grammar Multiple Choice"<< endl;
+	cout<< "here is the first question"<< endl;
+	cout<< "My sister was tired ____ she took a rest in a long time."<< endl;
+	cout<< "1) Because"<< endl;
+	cout<< "2) So"<< endl;
+	cout<< "3) But"<< endl;
+	cout<< endl;
+	cout<< "Write the number that you think the right answer = ";
+	cin >> nilai1 ;
+	cout << "____________________________________________" << endl;
+	if (nilai1 ==2) {
+	cout << "CORRECT! " << endl;
+}
+else{
+cout << "You're wrong! Try Again!'"<< endl;
+}
+cout<<endl;
+}
